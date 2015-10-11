d3b638fb419fc478296e9b3554d9ff38a614c16d
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2015-10-11 20:00:14 +0300

    fix ruby 2.0

--- lib/transactify.rb
+++ lib/transactify.rb
@@ -6,7 +6,7 @@ module Transactify
   def self.included(base_klass)
     base_klass.extend(ClassMethods)
     interceptor = const_set("#{base_klass.name.demodulize}Interceptor", Module.new)
-    base_klass.prepend(interceptor)
+    base_klass.send(:prepend, interceptor)
   end
 
   module ClassMethods
